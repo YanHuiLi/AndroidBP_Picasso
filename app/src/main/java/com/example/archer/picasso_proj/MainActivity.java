70ecdcd251102532d7f845ada280cf0f84b3a86b
Archer <421228286@qq.com>
2016-10-07 20:19:46 +0800

    picasso basic use

--- app/src/main/java/com/example/archer/picasso_proj/MainActivity.java
+++ app/src/main/java/com/example/archer/picasso_proj/MainActivity.java
@@ -1,52 +1,49 @@
 package com.example.archer.picasso_proj;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private ImageView imageView;
+    private final String IMAGE_PATH="http//img4.imgtn.bdimg.com/it/u=1301259139,2025033435&fm=21&gp=0.jpg";
+//    private final String IMAGE_PATH1="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSYWtnFjJ-GcfUfuBN2FLpi_azpyM8uoO4rEMKHlTvZEETyaoRv90lzipU";
+//    private final String IMAGE_PATH1="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSYWtnFjJ-GcfUfuBN2FLpi_azpyM8uoO4rEMKHlTvZEETyaoRv90lzipU";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        Button button = (Button) findViewById(R.id.button);
+        imageView= (ImageView) findViewById(R.id.imageView);
+
+        /**
+         * 图片下载的步骤
+         * 1.实用异步任务或者 handler+thread 获取图片资源
+         * 2.实用bitmapFactory 对图片进行解码
+         * 3.显示图片
+         *
+         * fit()方法应该是有一个BUG，当控件设置为wrap_content 的时候，无法加载图片
+         *
+         * 当控件设置为有一定的dp的长度时，fit方法可以加载，但是长度不对。
+         */
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                System.out.println("hello");
+                Picasso.with(MainActivity.this).load(IMAGE_PATH).resize(800,800).centerCrop().error(R.mipmap.ic_launcher).into(imageView);
+//                Picasso.with(MainActivity.this).load(IMAGE_PATH).fit().error(R.mipmap.ic_launcher).into(imageView);
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
